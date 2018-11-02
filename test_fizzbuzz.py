b61a98824b70ef15ed5940c51f861479885f3a75
rbbri <robin.s.johnson@gmail.com>
2018-11-02 13:57:06 +0000

    Refactored tests

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,19 +1,23 @@
 import unittest
+import random
 from fizzbuzz import Fizzbuzz
 
 
 class FizzbuzzTDD(unittest.TestCase):
 
     def test_fizzbuzz_returns_fizz_for_multiples_of_three(self):
-        result = Fizzbuzz().play(3)
+        rand = random.randint(1, 100)
+        result = Fizzbuzz().play(3 * rand)
         self.assertEqual(result, 'Fizz')
 
     def test_fizzbuzz_returns_buzz_for_multiples_of_five(self):
-        result = Fizzbuzz().play(5)
+        rand = random.randint(1, 100)
+        result = Fizzbuzz().play(5 * rand)
         self.assertEqual(result, 'Buzz')
 
     def test_fizzbuzz_returns_fizzbuzz_for_multiples_of_fifteen(self):
-        result = Fizzbuzz().play(15)
+        rand = random.randint(1, 100)
+        result = Fizzbuzz().play(15 * rand)
         self.assertEqual(result, 'FizzBuzz')
 
     def test_fizzbuzz_returns_the_number_otherwise(self):
